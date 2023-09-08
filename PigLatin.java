49b31f379014d219ee049cf5545430119aaf152c
kailyn0215 <brownkai24@7hills.org>
2023-09-08 13:08:40 -0400

    igpay atinlay uckssay

--- PigLatin.java
+++ PigLatin.java
@@ -4,14 +4,26 @@ import java.util.Scanner;
 
 public class PigLatin {
     public static void main(String[] args) {
-        String vowels = "aeiou"
-        String consonants = ""
-        String word = "";
-        len = word.length();
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+        String vowels = "aeiou";
+        String consonants = "";
+        int len = word.length();
+        for (int x = 0; x < len; x++)
+        {
+            if ((word[0]).equals(vowels[x]))
+            {
+                System.out.println(word + "way");
+            }
+        }
         for (int i = 0; i < len; i++)
         {
+            if (word[i].equals("a"))
+            {
+                System.out.println("found a");
+            }
             word.substring(i, i + 1); //up to but not including  i + 1
         }
-        word[i]
+        //word[i]
     }
 }

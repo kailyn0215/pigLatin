de9fb7dc28dc450475598ea483cda7ccc0e0caab
kailyn0215 <brownkai24@7hills.org>
2023-09-08 12:56:40 -0400

    doing coding !!!

---
+++ PigLatin.java
@@ -0,0 +1,17 @@
+// Kailyn Brown 9/8/23 Pig Latin Assignment
+
+import java.util.Scanner;
+
+public class PigLatin {
+    public static void main(String[] args) {
+        String vowels = "aeiou"
+        String consonants = ""
+        String word = "";
+        len = word.length();
+        for (int i = 0; i < len; i++)
+        {
+            word.substring(i, i + 1); //up to but not including  i + 1
+        }
+        word[i]
+    }
+}

7cce5ac5bdfd1c1399ff7e8686fd0f697f65220c
kailyn0215 <brownkai24@7hills.org>
2023-09-12 11:58:11 -0400

    added capital letter compatability :3

--- PigLatin.java
+++ PigLatin.java
@@ -13,13 +13,13 @@ public class PigLatin {
         char secondLetter = word.charAt(1); // assigns at position 2
 
         // if its a vowel
-        if (firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' || firstLetter == 'u')
+        if (firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' || firstLetter == 'u' || firstLetter == 'A' || firstLetter == 'E' || firstLetter == 'I' || firstLetter == 'O' || firstLetter == 'U')
         {
             System.out.println(word + "way"); // prints word with way @ end
         }
 
         // if a vowel is the second letter
-        else if (secondLetter == 'a' || secondLetter == 'e' || secondLetter == 'i' || secondLetter == 'o' || secondLetter == 'u' || secondLetter == 'y')
+        else if (secondLetter == 'a' || secondLetter == 'e' || secondLetter == 'i' || secondLetter == 'o' || secondLetter == 'u' || secondLetter == 'y' || secondLetter == 'A' || secondLetter == 'E' || secondLetter == 'I' || secondLetter == 'O' || secondLetter == 'U' || secondLetter == 'Y')
         {
             for (int x = 1; x < len; x++) // runs from second position to end
             {
